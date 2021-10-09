cf506ca3c27aeda47d9c7e3e8d362f8aeff4893c
Badriah Alshehri <badraihalshehri.cs@gmail.com>
2021-10-10 00:11:17 +0300

    Display StudentData from txt file

---
+++ ReadStudentData/ReadStudentData/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ReadStudentData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"C:\Users\beedo\OneDrive\سطح المكتب\SDA-Course\ReadStudentData\ReadStudentData\StudentData.txt";
+
+            //if (File.Exists(file))
+            //{
+            //    // Read all the content in one string
+            //    // and display the string
+            //    Console.WriteLine("EXists");
+            //    string str = File.ReadAllText(file);
+            //    Console.WriteLine(str);
+            //}
+
+            Console.WriteLine("Student Data");
+
+            // By using StreamReader
+            if (File.Exists(file))
+            {
+                // Reads file line by line
+                StreamReader Textfile = new StreamReader(file);
+                string line;
+
+                while ((line = Textfile.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+
+                Textfile.Close();
+
+                Console.ReadKey();
+            }
+            Console.WriteLine();
+        }
+
+
+    }
+}
